50170b81af72f21ede6a0d3fed522b171d03d2f8
danipereira261 <dpereira261@gmail.com>
2021-04-22 11:17:07 -0300

    Guardando qualquer referencia

--- src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,23 +1,26 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+
+        Object[] referencias = new Object[5];
+
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        contas[0] = cc1;
+        referencias[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-        contas[1] = cc2;
-
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        referencias[1] = cc2;
 
         //System.out.println(cc2.getNumero());
 
-        System.out.println( contas[1].getNumero()  );
+        //System.out.println(referencias[1].getNumero());
 
-        ContaCorrente ref = contas[1];
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; // type cast
         System.out.println(cc2.getNumero());
         System.out.println(ref.getNumero());
 
